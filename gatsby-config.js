5d7909dc39f6eb3bcd1032d517ce24102536a3e1
Janaka Abeywardhana <contact@janaka.co.uk>
2022-12-07 19:17:36 +0530

    chore: add Google Analytics
    using the same measurement ID as te fabrhq.com/

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,28 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-JL6KEWW836", // Google Analytics measurement ID
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          //respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          //exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-feed`,
       options: {
         query: `
